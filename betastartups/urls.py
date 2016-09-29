27d073674aa4b228aa1ffdc0bf79793e6056fb9a
donzigwe <uzoigweiroh@gmail.com>
2016-09-29 10:41:03 +0100

    Modified project URL

--- betastartups/urls.py
+++ betastartups/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf.urls import url, include, patterns
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^$', 'beta.views.index', name="index"),
